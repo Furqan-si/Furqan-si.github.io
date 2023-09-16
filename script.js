1c69d601e931ede04c4e084e42a5b0920391dc39
Furqan-si <145173785+Furqan-si@users.noreply.github.com>
2023-09-16 09:06:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function hitungLuas() {
+    var alas = parseFloat(document.getElementById("alas").value);
+    var tinggi = parseFloat(document.getElementById("tinggi").value);
+
+    if (!isNaN(alas) && !isNaN(tinggi)) {
+        var luas = 0.5 * alas * tinggi;
+        document.getElementById("hasilLuas").textContent = luas.toFixed(2);
+    } else {
+        alert("Masukkan panjang alas dan tinggi yang valid.");
+    }
+}
